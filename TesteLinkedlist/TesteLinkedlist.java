c3003ffada0f684af8cda5d26950f5fc7a27a529
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-04-17 10:59:27 -0300

    Create TesteLinkedlist.java

---
+++ TesteLinkedlist/TesteLinkedlist.java
@@ -0,0 +1,12 @@
+package teste;
+public class TesteLinkedlist {
+
+	public static void main(String[] args) {
+		Linkedlist l = new Linkedlist();
+		l.insertHead(10);
+		l.insertHead(5);
+		l.insertHead(1);
+		l.print();
+	}
+
+}
