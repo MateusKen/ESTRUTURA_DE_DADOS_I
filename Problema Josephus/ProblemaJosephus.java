912f73f7bc9d663d93551a605131d33c17b60dba
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-04-26 09:40:50 -0300

    Update ProblemaJosephus.java

--- Problema Josephus/ProblemaJosephus.java
+++ Problema Josephus/ProblemaJosephus.java
@@ -1,9 +1,10 @@
-import java.util.scanner
-  
+import java.util.Scanner;
+
 class ProblemaJosephus {
   public static void main(String[] args) {
     String opcoes = "\nMenu de Opções\n\n1 - Iniciar: cria uma lista vazia;\n2 - Inserir soldado\n3 - Sortear número\n4 - Retirar soldado\n5 - Mostrar os soldados\n6 - Encerra\n Opção:";
     Scanner ent = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     int opcao = 0;
     do{
       System.out.print(opcoes);
@@ -11,9 +12,19 @@ class ProblemaJosephus {
       switch(opcao){
         case 1:
           System.out.println("Iniciar: cria uma lista vazia");
+          CircleLinkedlist l = new CircleLinkedlist();
           break;
         case 2:
-          System.out.println("Inserir soldado");
+          System.out.println("Inserir soldado\n");
+          int codigo = 0;
+          String nome;
+          while (codigo != -1){
+          System.out.println("Digite o código do soldado (digitar -1 vai parar o loop):\n");
+          codigo = input.nextInt();
+          System.out.println("Digite o nome do soldado:\n");
+          nome = input.nextLine();
+          l.insertHead(codigo, nome);
+          }
           break;
         case 3:
           System.out.println("Sortear número");
